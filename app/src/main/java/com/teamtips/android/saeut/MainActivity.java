2a57c6bd36ee1e2784569f6b3cfa32ef4a9e0a1d
2jjingni <2jjingni@gmail.com>
2020-07-19 21:32:27 +0900

    first commit

---
+++ app/src/main/java/com/teamtips/android/saeut/MainActivity.java
@@ -0,0 +1,14 @@
+package com.teamtips.android.saeut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
