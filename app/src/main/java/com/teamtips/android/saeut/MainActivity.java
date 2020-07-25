b48926049435abdc99bfaf8b9c5e6ac37b9327fd
2jjingni <2jjingni@gmail.com>
2020-07-25 20:42:40 +0900

    ref : kakao map api key hash 문제 해결 중

--- app/src/main/java/com/teamtips/android/saeut/MainActivity.java
+++ app/src/main/java/com/teamtips/android/saeut/MainActivity.java
@@ -1,14 +1,16 @@
 package com.teamtips.android.saeut;
 
-import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Base64;
+import android.util.Log;
 import android.view.ViewGroup;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
         ViewGroup mapViewContainer = findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        getAppKeyHash();
+    }
+
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.teamtips.android.saeut", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("Hash key", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
     }
 }
