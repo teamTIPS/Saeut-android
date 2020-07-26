178ae62382629694c0855daa07bbadbb9b9cdcf2
2jjingni <2jjingni@gmail.com>
2020-07-26 15:18:23 +0900

    ref : kakao map api 연결 완료

--- app/src/main/java/com/teamtips/android/saeut/MainActivity.java
+++ app/src/main/java/com/teamtips/android/saeut/MainActivity.java
@@ -1,13 +1,21 @@
 package com.teamtips.android.saeut;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
@@ -17,27 +25,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
 
-        MapView mapView = new MapView(this);
-
-        ViewGroup mapViewContainer = findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
-
-        getAppKeyHash();
-    }
-
-    private void getAppKeyHash() {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo("com.teamtips.android.saeut", PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.e("Hash key", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+        Button button = findViewById(R.id.btnMap);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
             }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            Log.e("name not found", e.toString());
-        }
+        });
     }
 }
