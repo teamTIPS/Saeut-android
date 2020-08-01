34aa7000cef770924aa06cbb312c0d5b3edd7692
amazansia <asd111050@naver.com>
2020-08-01 23:21:58 +0900

    fix: Login function debug

---
+++ app/src/main/java/com/teamtips/android/saeut/ui/schedule/ScheduleViewModel.java
@@ -0,0 +1,19 @@
+package com.teamtips.android.saeut.ui.schedule;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ScheduleViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ScheduleViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is map fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
