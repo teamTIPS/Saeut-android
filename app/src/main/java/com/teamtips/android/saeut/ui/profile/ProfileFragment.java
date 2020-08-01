b15b67bba7b321c6d1095d0f54cb973a4b305043
2jjingni <2jjingni@gmail.com>
2020-08-01 14:33:11 +0900

    add : ViewPager 시도 중

--- app/src/main/java/com/teamtips/android/saeut/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/teamtips/android/saeut/ui/profile/ProfileFragment.java
@@ -23,11 +23,11 @@ public class ProfileFragment extends Fragment {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
+        //final TextView textView = root.findViewById(R.id.text_profile);
         profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+             //   textView.setText(s);
             }
         });
         return root;
