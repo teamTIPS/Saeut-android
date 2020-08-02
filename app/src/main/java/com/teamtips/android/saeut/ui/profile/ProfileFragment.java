7e1f4623dc30002175f6a145e76bff9d64587e9f
leejieuns2 <2jjingni@gmail.com>
2020-08-03 01:05:28 +0900

    hotfix : 구현 미완료 부분 주석 처리

--- app/src/main/java/com/teamtips/android/saeut/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/teamtips/android/saeut/ui/profile/ProfileFragment.java
@@ -38,8 +38,8 @@ public class ProfileFragment extends Fragment {
              //   textView.setText(s);
             }
         });
-        tv1 = root.findViewById(R.id.textView1);
-        tv2 = root.findViewById(R.id.textView2);
+//        tv1 = root.findViewById(R.id.textView1);
+//        tv2 = root.findViewById(R.id.textView2);
         NetworkTask networkTask = new NetworkTask("http://49.50.173.180:8080/saeut/account/test",null);
         networkTask.execute();
 
