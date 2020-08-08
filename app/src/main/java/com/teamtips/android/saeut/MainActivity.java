e6df2cf1e72015950c03b023056f07b059ef0546
daylight235 <no2pencil@naver.com>
2020-08-08 13:46:25 +0900

    add: new saeut ui

--- app/src/main/java/com/teamtips/android/saeut/MainActivity.java
+++ app/src/main/java/com/teamtips/android/saeut/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.util.Base64;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +18,8 @@ import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +27,17 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_notifications, R.id.navigation_dashboard, R.id.navigation_chat, R.id.navigation_profile)
-                .build();
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
         NavigationUI.setupWithNavController(navView, navController);
 
         getAppKeyHash();
+
     }
 
     private void getAppKeyHash() {
