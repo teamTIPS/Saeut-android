359eadee7468f537b704a9fb4ebfc3c43d3e1138
leejieuns2 <2jjingni@gmail.com>
2020-08-09 16:18:36 +0900

    add : dashboard & map 구현 사항 병합 완료

--- app/src/main/java/com/teamtips/android/saeut/dashboard/DashboardFragment.java
+++ app/src/main/java/com/teamtips/android/saeut/dashboard/DashboardFragment.java
@@ -6,7 +6,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,22 +13,9 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
-
 import com.google.android.material.tabs.TabLayout;
 import com.teamtips.android.saeut.R;
 import com.teamtips.android.saeut.TimberLogger;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.viewpager.widget.ViewPager;
-import com.google.android.material.tabs.TabLayout;
-import com.teamtips.android.saeut.R;
 
 
 public class DashboardFragment extends Fragment {
