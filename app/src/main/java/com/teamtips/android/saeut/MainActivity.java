05fd3445b4e1199ab08675a12c9589599c8285dc
amazansia <asd111050@naver.com>
2020-09-21 19:52:37 +0900

    fix: 로고화면 백그라운드에서 자동로그인 기능 작동 확인

--- app/src/main/java/com/teamtips/android/saeut/MainActivity.java
+++ app/src/main/java/com/teamtips/android/saeut/MainActivity.java
@@ -30,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, SplashActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, SplashActivity.class);
+//        startActivity(intent);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/teamtips/android/saeut/func/login/ui/generalLogin/SplashActivity.java
+++ app/src/main/java/com/teamtips/android/saeut/func/login/ui/generalLogin/SplashActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -23,20 +24,21 @@ public class SplashActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
     private static final String Tag = "SplashActivity";
+    boolean isloginSuccess = false; //true: 로그인 성공
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        String Rt = SaveSharedPreference.getRT(this);
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
+        startLoading();
+
+        String Rt = SaveSharedPreference.getRT(this);
         if(Rt != null && !Rt.equals("")){
             //Rt로 로그인하기
             loginViewModel.login(Rt);
         }
-        startLoading();
 
         loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
             @Override
@@ -45,32 +47,39 @@ public class SplashActivity extends AppCompatActivity {
                     Log.e(Tag,"loginResult == null");
                     return;
                 }
-
                 //로그인 실패
                 if (loginResult.getError() != null) {
                     Log.e(Tag,"SplashActivity 로그인 실패");
                     //로그인 결과 초기화
                     loginResult.setOrigin();
                     startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
                 }
                 //로그인 성공
                 if (loginResult.getSuccess() != null) {
-                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    isloginSuccess = true;
                     setResult(Activity.RESULT_OK);
-                    finish();
                 }
             }
         });
     }
+
     private void startLoading() {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Log.e(Tag,"시간 지연으로 로그인 실패");
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                finish();
+                if(isloginSuccess){
+                    Log.e(Tag,"자동로그인 성공");
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }
+                else {
+                    Log.e(Tag,"로그인 타임아웃, 로그인 화면으로 돌아갑니다.");
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
+                }
             }
-        }, 3000);
+        }, 2000);
     }
 }
