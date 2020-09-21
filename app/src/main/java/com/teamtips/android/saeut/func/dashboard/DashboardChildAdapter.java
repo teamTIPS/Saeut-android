fc3467cbef9b9b614dceebd94c623123a847cd22
daycoding <no2pencil@naver.com>
2020-09-22 00:30:06 +0900

    add: tag 색상변경

--- app/src/main/java/com/teamtips/android/saeut/func/dashboard/DashboardChildAdapter.java
+++ app/src/main/java/com/teamtips/android/saeut/func/dashboard/DashboardChildAdapter.java
@@ -95,10 +95,13 @@ public class DashboardChildAdapter extends BaseAdapter {
         int type = postArrayList.get(position).getType();
         if(type == 0) {
             holder.tagTitle.setText("장애인");
+            holder.tagTitle.setBackground(context.getResources().getDrawable(R.drawable.tag_handicap));
         } else if(type == 1) {
             holder.tagTitle.setText("아동");
+            holder.tagTitle.setBackground(context.getResources().getDrawable(R.drawable.tag_kid));
         } else {
             holder.tagTitle.setText("노인");
+            holder.tagTitle.setBackground(context.getResources().getDrawable(R.drawable.tag_elder));
         }
 
         // view 클릭 시 이벤트 처리
